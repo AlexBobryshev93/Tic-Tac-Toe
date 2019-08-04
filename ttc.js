71b7c342b98e1fde915423ffc6e3e30dcaa8df52
AlexBobryshev <darknessrider1@rambler.ru>
2019-08-05 00:24:30 +0300

    4th commit (computer opponents added)

--- ttc.js
+++ ttc.js
@@ -5,11 +5,15 @@ const labelImg = new Image();
 const xImg = new Image();
 const oImg = new Image();
 const box = 96; // square size (pxl)
-let turn;
+let turn = "X";
 let turnNumber = 1;
 let msg;
+let computerOpponent = true;
+//let computerOpponentTurn = setInterval(easyComputerOpponentTurn, 3000); // 3000 msec give the imitation of human thinking
+//let computerOpponentTurn = setInterval(normalComputerOpponentTurn, 3000); // 3000 msec give the imitation of human thinking
+let computerOpponentTurn = setInterval(hardComputerOpponentTurn, 3000); // 3000 msec give the imitation of human thinking
 
-let cells = [
+let cells = [ // we could leave it just undefined, but the strings were used for the debugging purposes
     ["[0][0]", "[0][1]", "[0][2]"],
     ["[1][0]","[1][1]", "[1][2]"],
     ["[2][0]", "[2][1]", "[2][2]"]
@@ -54,12 +58,19 @@ function nextTurn() {
         context.drawImage(labelImg, 112, 16);
         context.fillStyle = "black";
         context.fillText(msg, box * 2.5, 10 + box / 2);
-        canvas.removeEventListener("click", updateField);
+        if (turn == "X") canvas.removeEventListener("click", updateField);
+        clearInterval(computerOpponentTurn);
         return;
     }
 
-    if (turn == "X") turn = "O";
-    else if (turn == "O") turn = "X";
+    if (turn == "X") {
+        turn = "O";
+        if (computerOpponent) canvas.removeEventListener("click", updateField);
+    } else if (turn == "O") {
+        turn = "X";
+        if (computerOpponent) canvas.addEventListener("click", updateField);
+    }
+
     turnNumber++;
     msg = turn + " makes the turn";
     printMsg();
@@ -80,6 +91,7 @@ function checkWin() {
 
             printMsg();
             canvas.removeEventListener("click", updateField);
+            clearInterval(computerOpponentTurn);
             return true;
         }
 
@@ -96,6 +108,7 @@ function checkWin() {
 
             printMsg();
             canvas.removeEventListener("click", updateField);
+            clearInterval(computerOpponentTurn);
             return true;
         }
     }
@@ -112,6 +125,7 @@ function checkWin() {
         context.stroke();
 
         printMsg();
+        clearInterval(computerOpponentTurn);
         canvas.removeEventListener("click", updateField);
         return true;
     }
@@ -128,6 +142,7 @@ function checkWin() {
         context.stroke();
 
         printMsg();
+        clearInterval(computerOpponentTurn);
         canvas.removeEventListener("click", updateField);
         return true;
     }
@@ -137,13 +152,13 @@ function checkWin() {
 
 function startGame() {
     if (Math.floor(Math.random() * 2) == 1) turn = "X"; // who will be the first? 
-    else turn = "O";
+    else turn = "O"; 
     msg = turn + " makes the 1st turn";
 
     context.drawImage(fieldImg, 0, 0);
     context.font = "30px Times New Roman";
     printMsg();
-    canvas.addEventListener("click", updateField);
+    if (!computerOpponent || turn == "X") canvas.addEventListener("click", updateField);
 }
 
 function printMsg() {
@@ -151,5 +166,232 @@ function printMsg() {
     if (turn == "O") context.fillStyle = "red";
     else context.fillStyle = "blue";
     context.fillText(msg, box * 2, 10 + box / 2);
+}
+
+function easyComputerOpponentTurn() {
+    if (computerOpponent) {
+        if (turn == "O") {
+            let possibleX;
+            let possibleY;
+            do {
+                possibleX = Math.floor(Math.random() * 3);
+                possibleY = Math.floor(Math.random() * 3);
+            } while (cells[possibleX][possibleY] == "X" || cells[possibleX][possibleY] == "O");
+            
+            makeTurn(possibleX, possibleY);
+        }
+    }
+}
+
+function normalComputerOpponentTurn() {
+    if (computerOpponent) {
+        if (turn == "O") {
+            if (turnNumber == 1 || (turnNumber == 2 && cells[1][1] != "X")) {
+                makeTurn(1, 1);
+                return;
+            }
+
+            if (turnNumber > 3) { // plain defence against 3 in a row
+                for (let i = 0; i < 3; i++) { // against rows and columns
+                    if (cells[i][0] == "X" && cells[i][1] == "X" && cells[i][2] != "O") {
+                        makeTurn(i, 2);
+                        return;
+                    }
+                    if (cells[i][2] == "X" && cells[i][1] == "X" && cells[i][0] != "O") {
+                        makeTurn(i, 0);
+                        return;
+                    }
+                    if (cells[i][0] == "X" && cells[i][2] == "X" && cells[i][1] != "O") {
+                        makeTurn(i, 1);
+                        return;
+                    }
+                    if (cells[0][i] == "X" && cells[1][i] == "X" && cells[2][i] != "O") {
+                        makeTurn(2, i);
+                        return;
+                    }
+                    if (cells[2][i] == "X" && cells[1][i] == "X" && cells[0][i] != "O") {
+                        makeTurn(0, i);
+                        return;
+                    }
+                    if (cells[0][i] == "X" && cells[2][i] == "X" && cells[1][i] != "O") {
+                        makeTurn(1, i);
+                        return;
+                    }
+                }
+
+                if (cells[0][0] == "X" && cells[1][1] == "X" && cells[2][2] != "O") { // diagonal defence
+                    makeTurn(2, 2);
+                    return;
+                } 
+                if (cells[0][0] == "X" && cells[2][2] == "X" && cells[1][1] != "O") { // diagonal defence
+                    makeTurn(1, 1);
+                    return;
+                }
+                if (cells[1][1] == "X" && cells[2][2] == "X" && cells[0][0] != "O") { // diagonal defence
+                    makeTurn(0, 0);
+                    return;
+                }    
+                if (cells[0][2] == "X" && cells[1][1] == "X" && cells[2][0] != "O") { // diagonal defence
+                    makeTurn(2, 0);
+                    return;
+                } 
+                if (cells[0][2] == "X" && cells[2][0] == "X" && cells[1][1] != "O") { // diagonal defence
+                    makeTurn(1, 1);
+                    return;
+                }
+                if (cells[1][1] == "X" && cells[2][0] == "X" && cells[0][2] != "O") { // diagonal defence
+                    makeTurn(0, 2);
+                    return;
+                }
+            }
+
+            let possibleX;
+            let possibleY;
+            do {
+                possibleX = Math.floor(Math.random() * 3);
+                possibleY = Math.floor(Math.random() * 3);
+
+                
+            } while (cells[possibleX][possibleY] == "X" 
+                || cells[possibleX][possibleY] == "O" 
+                || (turnNumber == 2 && (possibleX + possibleY) % 2 != 0) // the second turn only through the center or diagonals
+            );
+            
+            makeTurn(possibleX, possibleY);
+        }
+    }
+}
+
+function hardComputerOpponentTurn() {
+    if (computerOpponent) {
+        if (turn == "O") {
+            if (turnNumber == 1 || (turnNumber == 2 && cells[1][1] != "X")) {
+                makeTurn(1, 1);
+                return;
+            }
+
+            if (turnNumber > 3) { // plain win 3 in a row
+                for (let i = 0; i < 3; i++) { // in rows and columns
+                    if (cells[i][0] == "O" && cells[i][1] == "O" && cells[i][2] != "X") {
+                        makeTurn(i, 2);
+                        return;
+                    }
+                    if (cells[i][2] == "O" && cells[i][1] == "O" && cells[i][0] != "X") {
+                        makeTurn(i, 0);
+                        return;
+                    }
+                    if (cells[i][0] == "O" && cells[i][2] == "O" && cells[i][1] != "X") {
+                        makeTurn(i, 1);
+                        return;
+                    }
+                    if (cells[0][i] == "O" && cells[1][i] == "O" && cells[2][i] != "X") {
+                        makeTurn(2, i);
+                        return;
+                    }
+                    if (cells[2][i] == "O" && cells[1][i] == "O" && cells[0][i] != "X") {
+                        makeTurn(0, i);
+                        return;
+                    }
+                    if (cells[0][i] == "O" && cells[2][i] == "O" && cells[1][i] != "X") {
+                        makeTurn(1, i);
+                        return;
+                    }
+                }
+
+                if (cells[0][0] == "O" && cells[1][1] == "O" && cells[2][2] != "X") { // diagonal
+                    makeTurn(2, 2);
+                    return;
+                } 
+                if (cells[0][0] == "O" && cells[2][2] == "O" && cells[1][1] != "X") { // diagonal
+                    makeTurn(1, 1);
+                    return;
+                }
+                if (cells[1][1] == "O" && cells[2][2] == "O" && cells[0][0] != "X") { // diagonal
+                    makeTurn(0, 0);
+                    return;
+                }    
+                if (cells[0][2] == "O" && cells[1][1] == "O" && cells[2][0] != "X") { // diagonal
+                    makeTurn(2, 0);
+                    return;
+                } 
+                if (cells[0][2] == "O" && cells[2][0] == "O" && cells[1][1] != "X") { // diagonal
+                    makeTurn(1, 1);
+                    return;
+                }
+                if (cells[1][1] == "O" && cells[2][0] == "O" && cells[0][2] != "X") { // diagonal
+                    makeTurn(0, 2);
+                    return;
+                }
+            }
+
+            if (turnNumber > 3) { // plain defence against 3 in a row
+                for (let i = 0; i < 3; i++) { // against rows and columns
+                    if (cells[i][0] == "X" && cells[i][1] == "X" && cells[i][2] != "O") {
+                        makeTurn(i, 2);
+                        return;
+                    }
+                    if (cells[i][2] == "X" && cells[i][1] == "X" && cells[i][0] != "O") {
+                        makeTurn(i, 0);
+                        return;
+                    }
+                    if (cells[i][0] == "X" && cells[i][2] == "X" && cells[i][1] != "O") {
+                        makeTurn(i, 1);
+                        return;
+                    }
+                    if (cells[0][i] == "X" && cells[1][i] == "X" && cells[2][i] != "O") {
+                        makeTurn(2, i);
+                        return;
+                    }
+                    if (cells[2][i] == "X" && cells[1][i] == "X" && cells[0][i] != "O") {
+                        makeTurn(0, i);
+                        return;
+                    }
+                    if (cells[0][i] == "X" && cells[2][i] == "X" && cells[1][i] != "O") {
+                        makeTurn(1, i);
+                        return;
+                    }
+                }
+
+                if (cells[0][0] == "X" && cells[1][1] == "X" && cells[2][2] != "O") { // diagonal defence
+                    makeTurn(2, 2);
+                    return;
+                } 
+                if (cells[0][0] == "X" && cells[2][2] == "X" && cells[1][1] != "O") { // diagonal defence
+                    makeTurn(1, 1);
+                    return;
+                }
+                if (cells[1][1] == "X" && cells[2][2] == "X" && cells[0][0] != "O") { // diagonal defence
+                    makeTurn(0, 0);
+                    return;
+                }    
+                if (cells[0][2] == "X" && cells[1][1] == "X" && cells[2][0] != "O") { // diagonal defence
+                    makeTurn(2, 0);
+                    return;
+                } 
+                if (cells[0][2] == "X" && cells[2][0] == "X" && cells[1][1] != "O") { // diagonal defence
+                    makeTurn(1, 1);
+                    return;
+                }
+                if (cells[1][1] == "X" && cells[2][0] == "X" && cells[0][2] != "O") { // diagonal defence
+                    makeTurn(0, 2);
+                    return;
+                }
+            }
+
+            let possibleX;
+            let possibleY;
+            do {
+                possibleX = Math.floor(Math.random() * 3);
+                possibleY = Math.floor(Math.random() * 3);
+
+                
+            } while (cells[possibleX][possibleY] == "X" 
+                || cells[possibleX][possibleY] == "O" 
+                || (turnNumber == 2 && (possibleX + possibleY) % 2 != 0) // the second turn only through the center or diagonals
+            );
+            
+            makeTurn(possibleX, possibleY);
+        }
+    }
 }
 
